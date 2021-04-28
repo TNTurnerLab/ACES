bbb2cd371a144c37e647b7aa7481232448924266
emehinovic72 <77067493+emehinovic72@users.noreply.github.com>
2021-04-28 12:45:30 -0500

    Update wgetfile_VGP.sh

--- VGP_SnakeFile_Pipeline/Genomes/wgetfile_VGP.sh
+++ VGP_SnakeFile_Pipeline/Genomes/wgetfile_VGP.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget --recursive --no-parent -A '*-unmasked.fa.gz' ftp://ftp.ensembl.org/pub/rapid-release/species/ && find ./Genomes -iname '*-unmasked.fa.gz*' -exec mv '{}' ./Genomes  \; && gunzip *-unmasked.fa.gz
+wget --recursive --no-parent -A '*-unmasked.fa.gz' ftp://ftp.ensembl.org/pub/rapid-release/species/ && find /Genomes -iname '*-unmasked.fa.gz*' -exec mv '{}' /Genomes  \; && gunzip *-unmasked.fa.gz
