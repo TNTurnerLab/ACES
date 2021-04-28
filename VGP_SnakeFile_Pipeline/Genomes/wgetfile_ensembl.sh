8406cd153137043aace989427d82c091407b1a74
emehinovic72 <77067493+emehinovic72@users.noreply.github.com>
2021-04-28 09:04:23 -0500

    Update wgetfile_ensembl.sh

--- VGP_SnakeFile_Pipeline/Genomes/wgetfile_ensembl.sh
+++ VGP_SnakeFile_Pipeline/Genomes/wgetfile_ensembl.sh
@@ -1,1 +1,2 @@
-wget --recursive --nH --no-parent -A '*.dna.toplevel.fa.gz' ftp://ftp.ensembl.org/pub/release-103/fasta/ && find ./Genomes -iname '*.dna.toplevel.fa.gz' -exec mv '{}' ./Genomes  \; && gunzip *.dna.toplevel.fa.gz
+#!/bin/bash
+wget --recursive --no-parent -A '*.dna.toplevel.fa.gz' ftp://ftp.ensembl.org/pub/release-103/fasta/ && find ./Genomes -iname '*.dna.toplevel.fa.gz' -exec mv '{}' ./Genomes  \; && gunzip *.dna.toplevel.fa.gz
