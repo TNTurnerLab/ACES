04cab2356357966bf6989a962c4f9e18d492aae3
jng2 <jng2@luc.edu>
2021-09-16 17:05:12 -0500

    [CODE] Deleted Snakemake Directory
    Deleted old snakemake directory

--- ACES_Pipeline/Genomes/wgetfile_VGP.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget --recursive --no-parent -A '*-unmasked.fa.gz' ftp://ftp.ensembl.org/pub/rapid-release/species/ && find ./ -iname '*-unmasked.fa.gz*' -exec mv '{}' ./  \; && gunzip *-unmasked.fa.gz
--- ACES_Pipeline/Genomes/wgetfile_ensembl.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-wget --recursive --no-parent -A '*.dna.toplevel.fa.gz' ftp://ftp.ensembl.org/pub/release-103/fasta/ && find ./ -iname '*.dna.toplevel.fa.gz' -exec mv '{}' ./  \; && gunzip *.dna.toplevel.fa.gz
